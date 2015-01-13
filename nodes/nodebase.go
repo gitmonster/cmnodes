4dd67fcc39160d36c33cf06c4ac470088cab63aa
denkhaus <peristaltic@gmx.net>
2015-01-13 01:29:21 +0100

    progress

--- nodes/nodebase.go
+++ nodes/nodebase.go
@@ -3,10 +3,12 @@ package nodes
 import "github.com/denkhaus/cmnodes/render"
 
 type NodeBase struct {
-	Id       string `bson:"_id"`
-	Parent   string `bson:"p"`
-	Order    int    `bson:"o"`
-	MimeType string `bson:"m"`
+	Id            string `bson:"_id"`
+	Parent        string `bson:"p"`
+	Order         int    `bson:"o"`
+	MimeType      string `bson:"m"`
+	RegisterRoute bool   `bson:"rr"`
+	Route         string `bson:"r"`
 
 	render *render.Render
 	engine *Engine
