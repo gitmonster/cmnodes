b19f999d857b6dce9aed4004a982dac830c1b43f
denkhaus <peristaltic@gmx.net>
2015-01-26 22:37:17 +0100

    progress

--- command/import.go
+++ command/import.go
@@ -11,15 +11,20 @@ func (c *Commander) NewInitProtosCommand() {
 		Usage: "Import command with subcommands",
 		Subcommands: []cli.Command{
 			{
-				Name:  "prototypes",
-				Usage: "Import prototypes",
+				Name:  "system",
+				Usage: "Import system node structure with object prototypes",
 				Action: func(ctx *cli.Context) {
+					var path string 
+					if len(ctx.Args()) > 0 {
+    					path = AbsPath(ctx.Args()[0])
+  					}
 					c.Execute(func(engine *nodes.Engine) error {
-						return nil //engine.ImportPrototypes(ctx.Bool("force"))
+						return nil engine.ImportSystem(ctx.Bool("force"),path)
 					}, ctx)
 				},
 				Flags: []cli.Flag{
-					cli.BoolFlag{"force, f", "delete all prototypes in database before importing", ""},
+					cli.BoolFlag{"force, f", "Remove any existing system structure before importing.", ""},
+					cli.BoolFlag{"path, p", "Path of the system toml file. When empty the app looks for system.toml in the current directory.", ""},
 				},
 			},
 		},
