77690b27f2716f88fde03276ca06c651b9f1c7a4
denkhaus <peristaltic@gmx.net>
2015-02-03 00:16:19 +0100

    progress

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -2,12 +2,23 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         'connect': {
-            demo: {
+            server: {
                 options: {
                     open: true,
-                    keepalive: true
+                    keepalive: true,
+                    port: 8000,
+                    base: '.'
                 }
             }
+        },
+        'watch': {
+            options: {
+                livereload: true,
+            },
+            html: {
+                files: ['index.html', 'src/**/*', '!dist'],
+                tasks: ['build'],
+            },
         },
         'gh-pages': {
             options: {
@@ -19,24 +30,51 @@ module.exports = function(grunt) {
                 'demo/*', 'src/*', 'index.html'
             ]
         },
+        'copy': {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components',
+                    src: ['**/*'],
+                    dest: 'dist/vendor/'
+                }],
+            },
+        },
         'replace': {
-            example: {
-                src: ['src/*'],
+            index: {
+                src: ['index.html'],
                 dest: 'dist/',
                 replacements: [{
                     from: 'bower_components',
-                    to: '..'
+                    to: 'vendor'
+                }, {
+                    from: /href=\"src/g,
+                    to: 'href=\"modules'
+                }]
+            },
+            modules: {
+                src: ['src/**/*'],
+                dest: 'dist/modules/',
+                replacements: [{
+                    from: 'bower_components',
+                    to: 'vendor'
                 }]
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('build',  ['replace']);
+    grunt.registerTask('build', ['replace', 'copy']);
     grunt.registerTask('deploy', ['gh-pages']);
-    grunt.registerTask('server', ['connect']);
+    grunt.registerTask('default', ['build', 'connect', 'watch']);
+
+    grunt.event.on('watch', function(action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
 
 };
